f52d53b2ce7cdedf946025e0d23c9c362b59b058
baladotdextserv <bala.dextserv@gmail.com>
2025-02-19 18:24:16 +0530

    firebase added

---
+++ app/Component/Home.tsx
@@ -0,0 +1,31 @@
+// pages/index.js
+'use client'
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore'; // Firestore functions
+import { db } from '@/firebase/config';
+
+const Home = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [data, setData] = useState<any>([]);
+
+  console.log(data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, 'today'));
+      const documents = querySnapshot.docs.map(doc => doc.data());
+      console.log(documents);
+      setData(documents);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from Firestore</h1>
+    </div>
+  );
+};
+
+export default Home;
